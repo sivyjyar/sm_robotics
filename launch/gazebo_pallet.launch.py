c768bcd0a338681e9ffb88d5393ed22fa6bb2926
Yaroslav <khodakovsky1@bk.ru>
2024-09-21 20:31:43 +0300

    Модель погрузчика с цилиндрическим корпусом и возможностью управлять поворотом колеса и скоростью. Самонаправляющиеся колеса

---
+++ launch/gazebo_pallet.launch.py
@@ -0,0 +1,102 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+from launch_ros.actions import SetParameter
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'sm_robotics'
+
+    set_use_sim_time = SetParameter(name='use_sim_time', value=True)
+
+   # using old launch file to launch robot_state_publisher with pallet robot
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(pkg_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time':'True'}.items()
+    )
+
+
+
+    # including gazebo launch file
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch',
+            'gazebo.launch.py')]),
+    )
+
+    #run a node for spawning pallet robot
+    spawn_entity = Node(package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','robot_description',
+                   '-entity','pallet_robot'],
+        output='screen')
+
+
+
+# # CREATING CONTROLERS
+    controller_params_file = os.path.join(get_package_share_directory(pkg_name),'config','controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[controller_params_file]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_state_broadcaster",
+                       "--controller-manager", "/controller_manager"],
+        )
+    #
+    # steering_spawner = Node(
+    #         package="controller_manager",
+    #         executable="spawner",
+    #         arguments=["steering_controller",
+    #                    "--controller-manager", "/controller_manager"],
+    #     )
+    #
+    # drive_wheel_spawner = Node(
+    #         package="controller_manager",
+    #         executable="spawner",
+    #         arguments=["drive_wheel_controller",
+    #                    "--controller-manager", "/controller_manager"],
+    #     )
+    #
+
+    robot_controller_spawner = Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["steering_controller",
+                       "drive_wheel_controller"],
+        )
+
+
+    # Run the node
+    return LaunchDescription([
+        set_use_sim_time,
+        rsp,
+        gazebo,
+        spawn_entity,
+
+        # controller_manager,  # - нужно будет подключать при переносе на реального робота(?)
+
+        joint_state_broadcaster_spawner,
+
+        robot_controller_spawner
+
+        # steering_spawner,
+        # drive_wheel_spawner,
+
+
+    ])
+
---
+++ teleop_to_movement.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import sys
+import threading
+import rclpy
+
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64MultiArray
+
+
+class TeleopController(Node):
+    def __init__(self):
+        super().__init__('teleop_controller')
+
+        # Подписываемся на топик /cmd_vel
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.cmd_vel_callback,
+            10)
+
+        # Публикуем в топики управления джойнтами
+        self.steering_publisher = self.create_publisher(Float64MultiArray, '/steering_controller/commands', 10)
+        self.drive_publisher = self.create_publisher(Float64MultiArray, '/drive_wheel_controller/commands', 10)
+
+    def cmd_vel_callback(self, msg):
+        # Извлекаем линейную и угловую скорость
+        linear_velocity = msg.linear.x  # Скорость движения вперед/назад
+        angular_velocity = msg.angular.z  # Скорость поворота
+
+        # Публикуем команды для управления джойнтами
+
+        steering_msg = Float64MultiArray()
+        steering_msg.data = [angular_velocity]
+
+        drive_msg = Float64MultiArray()
+        drive_msg.data = [linear_velocity]  # Движение мотор-колеса
+        # print(msg.data)
+
+        self.steering_publisher.publish(steering_msg)
+        self.drive_publisher.publish(drive_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    teleop_controller = TeleopController()
+    rclpy.spin(teleop_controller)
+    teleop_controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
